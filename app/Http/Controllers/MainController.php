2ccd462214ae40da36041848c2880a2077054b12
Glessme <roger02michel@outlook.fr>
2024-01-29 16:47:53 +0100

    récupération de la classe actu et création de Actu dans models

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Actu;
 use Illuminate\Http\Request;
 
 class Maincontroller extends Controller
 {
-    public function base() {
-        return view('base');
-    }
+    public function base() 
+        {
+            $actus = Actu::all();// Récupération de toutes les instances de la classe Actu (Actu dans Models).
+            // Retourne la vue 'home' avec les données passées en paramètre.
+            // Dans ce cas, on passe les résultats de la récupération des actualités (actus) à la vue.
+            return view('base', [
+                'actus' => $actus,
+    
+            ]);
+        }
 }
